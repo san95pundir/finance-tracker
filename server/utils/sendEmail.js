4ca91dc00782fa0c18ee4b5a9490845547af4453
sanyapundir02 <sanya9a05@gmail.com>
2026-07-08 18:53:27 +0530

    fix IPv6 connection issue in email sending

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -2,11 +2,14 @@ const nodemailer = require('nodemailer');
 
 const sendEmail = async (to, subject, html) => {
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS
-    }
+    },
+    family: 4  // IPv4 force karo, IPv6 use mat karo
   });
 
   await transporter.sendMail({
